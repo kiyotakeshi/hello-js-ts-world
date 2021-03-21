8823288e15a0b833eb602095991bbe91af4b3abb
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-22 01:34:17 +0900

    polish arrow function

--- props/src/CharacterList.tsx
+++ props/src/CharacterList.tsx
@@ -15,34 +15,29 @@ type Props = {
 };
 
 // FC に型引数を渡すことで、そのコンポーネントの props の型を指定できる
-// props の値を第一引数 props として受け取ることができる
-const CharacterList: FC<Props> = (props) => {
-    const { school, characters } = props;
-    console.log(characters);
-
-    // 関数コンポーネントではレンダリングしたい内容を戻り値として return で返す
-    // FC 型で戻り値の型として定義されているのは ReactElement か null(何もレンダリングしない)
-    return (
-        // フラグメント(JSX では最上位階層の要素は一つでないといけないが、意味のない <div> 階層を作りたくない時に使用)
-        <>
-            <Header as="h2">{school}</Header>
-            <Item.Group>
-              {characters.map((character) => (
+// props から school, characters を抽出する分割代入の処理を引数の受け渡しに移した
+// アロー関数の中身に return 文しかないので return を省略
+const CharacterList: FC<Props> = ({ school, characters }) => (
+    // フラグメント(JSX では最上位階層の要素は一つでないといけないが、意味のない <div> 階層を作りたくない時に使用)
+    <>
+        <Header as="h2">{school}</Header>
+        <Item.Group>
+            {characters.map((character) => (
                 <Item key={character.id}>
-                  <Icon name="user circle" size="huge"/>
-                  <Item.Content>
-                    <Item.Header>{character.name}</Item.Header>
-                    <Item.Meta>{character.grade}年生</Item.Meta>
-                    <Item.Meta>
-                      {/* 設定されていなければ参照値は undefined なので '???' が返る */}
-                      {character.height ? character.height : '???'}cm
-                    </Item.Meta>
-                  </Item.Content>
+                    <Icon name="user circle" size="huge" />
+                    <Item.Content>
+                        <Item.Header>{character.name}</Item.Header>
+                        <Item.Meta>{character.grade}年生</Item.Meta>
+                        <Item.Meta>
+                            {/* 設定されていなければ参照値は undefined なので '???' が返る */}
+                            {/* nullish coalescing で記述 */}
+                            {character.height ?? '???'}cm
+                        </Item.Meta>
+                    </Item.Content>
                 </Item>
-              ))}
-            </Item.Group>
-        </>
-    );
-};
+            ))}
+        </Item.Group>
+    </>
+);
 
 export default CharacterList;
