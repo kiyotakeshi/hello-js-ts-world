fbef3d35678e9038f495004dddc4bfd8ad6935ae
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-22 11:08:39 +0900

    rewrite to class component

--- props/src/App.tsx
+++ props/src/App.tsx
@@ -1,9 +1,59 @@
-import { FC } from 'react'; // FC は FunctionComponent
+// import { FC } from 'react'; // FC は FunctionComponent
+import { Component, ReactElement } from 'react';
 import CharacterList, { Character } from './CharacterList'; // Character は型エイリアス
 import './App.css';
 
-const App: FC = () => {
-    const characters: Character[] = [
+// const App: FC = () => {
+//     const characters: Character[] = [
+//         {
+//             id: 1,
+//             name: '桜木花道',
+//             grade: 1,
+//             height: 189.2,
+//         },
+//         {
+//             id: 2,
+//             name: '流川 楓',
+//             grade: 1,
+//             height: 187,
+//         },
+//         {
+//             id: 3,
+//             name: '宮城リョータ',
+//             grade: 2,
+//             height: 168,
+//         },
+//         {
+//             id: 4,
+//             name: '三井 寿',
+//             grade: 3,
+//         },
+//         {
+//             id: 5,
+//             name: '赤木剛憲',
+//             grade: 3,
+//             height: 197,
+//         },
+//     ];
+
+//     return (
+//         <div className="container">
+//             <header>
+//                 <h1>登場人物</h1>
+//             </header>
+//             {/* 属性が props として子コンポーネントである CharacterList に渡される */}
+//             <CharacterList school="高校" characters={characters} />
+//         </div>
+//     );
+// };
+
+// class component
+// React が提供する Component を継承
+class App extends Component {
+
+    // characters がクラスのメンバー変数として定義
+    // TypeScript のプロパティ初期化子
+    characters: Character[] = [
         {
             id: 1,
             name: '桜木花道',
@@ -35,15 +85,18 @@ const App: FC = () => {
         },
     ];
 
-    return (
+    // メンバーメソッド render() の戻り値がレンダリング対象
+    render(): ReactElement {
+      return(
         <div className="container">
-            <header>
-                <h1>登場人物</h1>
-            </header>
-            {/* 属性が props として子コンポーネントである CharacterList に渡される */}
-            <CharacterList school="高校" characters={characters} />
+          <header>
+            <h1>登場人物</h1>
+          </header>
+          <CharacterList school="高校" characters={this.characters} />
         </div>
-    );
-};
+      )
+    }
+
+}
 
 export default App;
--- props/src/CharacterList.tsx
+++ props/src/CharacterList.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+// import { FC } from 'react';
+import { Component, ReactElement } from 'react';
 import { Header, Icon, Item } from 'semantic-ui-react';
 
 // 型エイリアス
@@ -14,30 +15,59 @@ type Props = {
     characters: Character[];
 };
 
-// FC に型引数を渡すことで、そのコンポーネントの props の型を指定できる
-// props から school, characters を抽出する分割代入の処理を引数の受け渡しに移した
-// アロー関数の中身に return 文しかないので return を省略
-const CharacterList: FC<Props> = ({ school, characters }) => (
-    // フラグメント(JSX では最上位階層の要素は一つでないといけないが、意味のない <div> 階層を作りたくない時に使用)
-    <>
-        <Header as="h2">{school}</Header>
-        <Item.Group>
-            {characters.map((character) => (
-                <Item key={character.id}>
-                    <Icon name="user circle" size="huge" />
-                    <Item.Content>
-                        <Item.Header>{character.name}</Item.Header>
-                        <Item.Meta>{character.grade}年生</Item.Meta>
-                        <Item.Meta>
-                            {/* 設定されていなければ参照値は undefined なので '???' が返る */}
-                            {/* nullish coalescing で記述 */}
-                            {character.height ?? '???'}cm
-                        </Item.Meta>
-                    </Item.Content>
-                </Item>
-            ))}
-        </Item.Group>
-    </>
-);
+// // FC に型引数を渡すことで、そのコンポーネントの props の型を指定できる
+// // props から school, characters を抽出する分割代入の処理を引数の受け渡しに移した
+// // アロー関数の中身に return 文しかないので return を省略
+// const CharacterList: FC<Props> = ({ school, characters }) => (
+//     // フラグメント(JSX では最上位階層の要素は一つでないといけないが、意味のない <div> 階層を作りたくない時に使用)
+//     <>
+//         <Header as="h2">{school}</Header>
+//         <Item.Group>
+//             {characters.map((character) => (
+//                 <Item key={character.id}>
+//                     <Icon name="user circle" size="huge" />
+//                     <Item.Content>
+//                         <Item.Header>{character.name}</Item.Header>
+//                         <Item.Meta>{character.grade}年生</Item.Meta>
+//                         <Item.Meta>
+//                             {/* 設定されていなければ参照値は undefined なので '???' が返る */}
+//                             {/* nullish coalescing で記述 */}
+//                             {character.height ?? '???'}cm
+//                         </Item.Meta>
+//                     </Item.Content>
+//                 </Item>
+//             ))}
+//         </Item.Group>
+//     </>
+// );
+
+// class component
+class CharacterList extends Component<Props> {
+    render(): ReactElement {
+        // 関数コンポーネントのように関数の引数として props を渡せないので、 this.props に格納されているのを取り出す
+        // コンストラクタで初期化をおこなう必要があるが、初期化する処理がなかったので省略
+        const { school, characters } = this.props;
+
+        return (
+            <>
+                <Header as="h2">{school}</Header>
+                <Item.Group>
+                    {characters.map((character) => (
+                        <Item>
+                            <Icon name="user circle" size="huge" />
+                            <Item.Content>
+                                <Item.Header>{character.name}</Item.Header>
+                                <Item.Meta>{character.grade}</Item.Meta>
+                                <Item.Meta>
+                                    {character.height ?? '???'}cm
+                                </Item.Meta>
+                            </Item.Content>
+                        </Item>
+                    ))}
+                </Item.Group>
+            </>
+        );
+    }
+}
 
 export default CharacterList;
